e2ad7c14988560c2fc5491e1733ca429a92173ac
Matteo <matteomigliori280489@gmail.com>
2021-03-09 10:01:01 +0100

    terzo push da sistemare style

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 
+    
+    $('.with-dropdown').mouseover(function() { 
+        $(this).find('.dropdown-menu').addClass('view-dropdown'); 
+    })
 
+    $('.dropdown-menu').mouseleave(function(){
+        $('.dropdown-menu').removeClass('view-dropdown');
 
-
+    })
+   
 
 });
 
